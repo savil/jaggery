f6d3d6394f7fb4104c762a7ee83a66ce673290dd
Savil Srivastava <savil@cs.stanford.edu>
2021-11-01 06:22:59 -0700

    hash dataAry to commit, and store in merkleTrie.values

--- merkle-trie/src/main.zig
+++ merkle-trie/src/main.zig
@@ -13,13 +13,13 @@ const Allocator = std.mem.Allocator;
 const merkleTrie = struct {
 
     const Self = @This();
+    const hasher = std.crypto.hash.blake2.Blake2b256;
 
-    values: std.ArrayList([]u8),
+    values: std.ArrayList([hasher.digest_length]u8),
 
     pub fn init(allocator: *Allocator) Self {
-        const list = std.ArrayList([]u8).init(allocator);
         return Self{
-            .values = list
+            .values = std.ArrayList([hasher.digest_length]u8).init(allocator),
         };
     }
 
@@ -27,8 +27,15 @@ const merkleTrie = struct {
         self.values.deinit();
     }
 
-    pub fn commit() void {
+    pub fn commit(self: *Self, dataAry: [] const []const u8) !void {
+        _ = self;
+        _ = dataAry;
 
+        for (dataAry) |datum| {
+            var hashed_datum: [hasher.digest_length]u8 = undefined;
+            hasher.hash(datum, &hashed_datum, .{});
+            try self.values.append(hashed_datum); 
+        }
     }
 
     pub fn open() void {
@@ -44,4 +51,12 @@ const merkleTrie = struct {
 test "init empty merkle trie" {
     const m = merkleTrie.init(testing.allocator);
     defer m.deinit();
+}
+
+test "commit merkle trie" {
+    var m = merkleTrie.init(testing.allocator);
+    defer m.deinit();
+
+    const dataAry = [2][]const u8{"savil", "diana"};
+    try m.commit(dataAry[0..]);
 }
