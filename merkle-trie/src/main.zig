3b0a4840c9bff3db6e56380d0f8a47e3f472f365
Savil Srivastava <savil@cs.stanford.edu>
2021-11-03 06:21:09 -0700

    add trie-nodes by hashing child elements

--- merkle-trie/src/main.zig
+++ merkle-trie/src/main.zig
@@ -2,12 +2,6 @@ const std = @import("std");
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
-//fn newMerkleTrie() merkleTrie {
- //   return merkleTrie{
- //       .values = nil
-  //  };
-//}
-
 /// fun inspiration:
 /// https://aszepieniec.github.io/stark-anatomy/basic-tools#merkle-tree
 const merkleTrie = struct {
@@ -27,6 +21,9 @@ const merkleTrie = struct {
         self.values.deinit();
     }
 
+    /// commit eagerly builds a balanced binary trie. This is not space-efficient
+    /// but is simpler for me to reason about :-). Optimizations can be made later
+    /// once this works.
     pub fn commit(self: *Self, dataAry: [] const []const u8) !void {
         _ = self;
         _ = dataAry;
@@ -35,6 +32,29 @@ const merkleTrie = struct {
             var hashed_datum: [hasher.digest_length]u8 = undefined;
             hasher.hash(datum, &hashed_datum, .{});
             try self.values.append(hashed_datum); 
+        }
+        try self.ensureValuesLengthIsPowerOfTwo();
+
+
+        var level_start_idx: u64 = 0;
+        var level_idx: u64 = 0;
+        var level_len: u64 = self.values.items.len;
+        while (level_len >= 2) {
+            while (level_idx < level_len) {
+                var current_idx = level_start_idx + level_idx;
+                var buf = self.copyToBuf(current_idx);
+                var hashed_datum: [hasher.digest_length]u8 = undefined;
+                hasher.hash(buf[0..], &hashed_datum, .{});
+                try self.values.append(hashed_datum); 
+
+                // copyToBuf processes two array elements, 
+                // so we move ahead by two.
+                level_idx += 2; 
+            }
+
+            level_start_idx += level_len;
+            level_idx = 0;
+            level_len = level_len / 2;
         }
     }
 
@@ -46,6 +66,38 @@ const merkleTrie = struct {
 
     }
 
+    fn copyToBuf(self: *Self, cp_idx: u64) []const u8 {
+
+        var buf: [2* hasher.digest_length] u8 = undefined;
+        var idx: u64 = 0;
+        for (self.values.items[cp_idx]) | char | {
+            buf[idx] = char;
+            idx += 1;
+        }
+        for (self.values.items[cp_idx + 1]) | char | {
+            buf[idx] = char;
+            idx += 1;
+        }
+        return buf[0..];
+    }
+
+    fn ensureValuesLengthIsPowerOfTwo(self: *Self) !void {
+        while (!self.valuesLengthIsPowerOfTwo()) {
+            const datum = "";
+            var hashed_datum: [hasher.digest_length]u8 = undefined;
+            hasher.hash(datum, &hashed_datum, .{});
+            try self.values.append(hashed_datum);
+        }
+    }
+
+    // https://bits.stephan-brumme.com/isPowerOfTwo.html
+    fn valuesLengthIsPowerOfTwo(self: *Self) bool {
+        const lenVals = self.values.items.len;
+        if (lenVals < 2) {
+            return true;
+        }
+        return (lenVals & (lenVals - 1)) == 0;
+    }
 };
 
 test "init empty merkle trie" {
@@ -53,10 +105,18 @@ test "init empty merkle trie" {
     defer m.deinit();
 }
 
-test "commit merkle trie" {
+test "empty merkle trie" {
+    var m = merkleTrie.init(testing.allocator);
+    defer m.deinit();
+    
+    const dataAry = [_][]const u8{};
+    try m.commit(dataAry[0..]);
+}
+
+test "(small) merkle trie" {
     var m = merkleTrie.init(testing.allocator);
     defer m.deinit();
 
-    const dataAry = [2][]const u8{"savil", "diana"};
+    const dataAry = [4][]const u8{"savil", "diana", "noorjahan", "ruksaar"};
     try m.commit(dataAry[0..]);
 }
